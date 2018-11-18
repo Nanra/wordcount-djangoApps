ea3184c9e04740349c61c9ac03f98d379088b573
Nanra <sukedynanra@gmail.com>
2018-11-19 04:27:03 +0700

    Ad count word and number of words feature

--- wordcount/views.py
+++ wordcount/views.py
@@ -5,4 +5,20 @@ def home(request) :
     return render(request, 'home.html')
 
 def count(request) :
-    return render(request, 'count.html')
+    fulltext = request.GET['fulltext']
+    print(fulltext)
+    wordlist = fulltext.split()
+
+    wordDictionary = {}
+
+    for word in wordlist:
+        if word in wordDictionary:
+            wordDictionary[word] += 1
+        else:
+            wordDictionary[word] = 1
+
+    return render(
+    request, 'count.html',
+    {'text': fulltext,
+    'textCount': len(wordlist),
+    'kamusKata': wordDictionary.items()})
