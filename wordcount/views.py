1f8d3ca5e69e86b3dc86d1a96d38c262a5fa338f
Nanra <sukedynanra@gmail.com>
2018-11-19 03:38:32 +0700

    Add URLS Count

--- wordcount/views.py
+++ wordcount/views.py
@@ -2,4 +2,7 @@
 from django.shortcuts import render
 
 def home(request) :
-    return render (request, 'home.html')
+    return render(request, 'home.html')
+
+def count(request) :
+    return render(request, 'count.html')
